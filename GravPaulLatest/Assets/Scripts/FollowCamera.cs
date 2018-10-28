2a9ebe592824c7cb29926d87f46356e2cd7b457b
DESKTOP-0J68V8V\Sam <sronning@umich.edu>
2018-10-27 23:27:55 -0400

    Changelog:
    [Art]
    ++Backgound Space
    ++Window and background tilemap

---
+++ GravPaulLatest/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public Transform tgt;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float x = tgt.position.x;
+        float y = tgt.position.y;
+        transform.position = new Vector3(x, y, 0);
+	}
+}
